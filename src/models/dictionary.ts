ec6d904c92cb143d4101edae97a8c1158ca2e045
趙偉 <zwvista@msn.com>
2018-10-27 07:15:00 +0900

    http

--- src/models/dictionary.ts
+++ src/models/dictionary.ts
@@ -10,8 +10,7 @@ class Dictionary {
   TEMPLATE!: string;
 
   urlString(word: string): string {
-    const url = this.URL.replace('{0}', encodeURIComponent(word))
-      .replace('http://', 'https://');
+    const url = this.URL.replace('{0}', encodeURIComponent(word));
     console.log(url);
     return url;
   }
