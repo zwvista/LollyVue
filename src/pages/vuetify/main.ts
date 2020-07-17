36e6d27bf0e6dab36ebc6402fd01b453d0974196
zwvista <zwvista@msn.com>
2020-07-17 12:04:22 +0900

    core-js

--- src/pages/vuetify/main.ts
+++ src/pages/vuetify/main.ts
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import Vue from 'vue';
 import vuetify from '@/plugins/plugin-vuetify';
 import '@/plugins/plugin-vue-typescript-inject';
